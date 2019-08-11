fb5328ba267654f5da1ecbdf7bb7fb606767af72
Flizberg <53863339+Flizberg@users.noreply.github.com>
2019-08-11 17:51:23 +0300

    Add files via upload

---
+++ Урок 1/Глава 2/var3.py
@@ -0,0 +1,12 @@
+import math
+
+a=int(input())  #ввели а
+b=int(input())
+x=int(input())
+y=int(input())
+f=3*(a+b*b)/(x-7*math.sqrt(y))
+f=(2*a*a+5*b)/(math.fabs(x+y))
+f=2*(a+b*b)*(math.fabs(x-7*y))+a*b
+print(f)
+
+
